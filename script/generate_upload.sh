f6af5f01858d9a2e6f3345764dc7c73ea6fa6ff1
anne <honghonghunter@hotmail.com>
2023-08-13 12:56:49 +1200

    上善若水

--- script/generate_upload.sh
+++ script/generate_upload.sh
@@ -6,7 +6,7 @@ file_path="./script/story_original.txt"
 csv_key="./s3/index.csv"
 title_chinese=$(head -n 1 "$file_path")
 title_english=$(sed -n '2p' "$file_path")
-story_chinese=$(sed -n '3p' "$file_path")
+story_chinese=$(sed -n '3,$p' "$file_path" | tr -d '[:space:]' | tr -d '\n')
 story_english=$(aws translate translate-text --text "$story_chinese" --source-language-code zh --target-language-code en --query 'TranslatedText' --output text)
 story_french=$(aws translate translate-text --text "$story_chinese" --source-language-code zh --target-language-code fr --query 'TranslatedText' --output text)
 # Download existing books and update them later
