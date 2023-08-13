ece0d1aafc48bade49fc3943e767acb331fda78b
anne <honghonghunter@hotmail.com>
2023-08-14 07:03:05 +1200

    backup s3 object before modifying

--- script/generate_upload.sh
+++ script/generate_upload.sh
@@ -4,6 +4,7 @@ bucket_name='everyday-story'
 echo "pwd is: $(pwd)"
 file_path="./script/story_original.txt"
 csv_key="./downloads/index.csv"
+local_prefix="./downloads"
 title_chinese=$(head -n 1 "$file_path")
 title_english=$(sed -n '2p' "$file_path")
 story_chinese=$(sed -n '3,$p' "$file_path" | tr -d '[:space:]' | tr -d '\n')
@@ -46,9 +47,9 @@ generate_books() {
         title="${titles[$i]}"
         story="${stories[$i]}"
 
-        echo "The title is: $title" >> "./downloads/books/${lang}_chengyu.txt"
-        echo "The story content is: $story" >> "./downloads/books/${lang}_chengyu.txt"
-        echo "$break_line" >> "./downloads/books/${lang}_chengyu.txt"
+        echo "The title is: $title" >> "${local_prefix}/books/${lang}_chengyu.txt"
+        echo "The story content is: $story" >> "${local_prefix}/books/${lang}_chengyu.txt"
+        echo "$break_line" >> "${local_prefix}/books/${lang}_chengyu.txt"
     done
 }
 generate_books
@@ -68,7 +69,7 @@ create_json_file() {
     }'
 
     # Write the JSON content to the file
-    echo "$meta_content" > "./s3/story/"$story_name_metadata".json"
+    echo "$meta_content" > "${local_prefix}/story/"$story_name_metadata".json"
     echo "Generated the metadata json file."
 }
 create_json_file
@@ -77,28 +78,28 @@ create_json_file
 
 generate_speeches() {
     echo "Generating story speeches."
-    aws polly synthesize-speech --text "$story_chinese" --output-format mp3 --voice-id Zhiyu --sample-rate 16000 "./s3/story/"${story_name_chinese}.mp3
-    aws polly synthesize-speech --text "$story_english" --output-format mp3 --voice-id Matthew --sample-rate 16000 "./s3/story/"${story_name_english}.mp3
-    aws polly synthesize-speech --text "$story_french" --output-format mp3 --voice-id Celine --sample-rate 16000 "./s3/story/"${story_name_french}.mp3
+    aws polly synthesize-speech --text "$story_chinese" --output-format mp3 --voice-id Zhiyu --sample-rate 16000 "${local_prefix}/story/"${story_name_chinese}.mp3
+    aws polly synthesize-speech --text "$story_english" --output-format mp3 --voice-id Matthew --sample-rate 16000 "${local_prefix}/story/"${story_name_english}.mp3
+    aws polly synthesize-speech --text "$story_french" --output-format mp3 --voice-id Celine --sample-rate 16000 "${local_prefix}/story/"${story_name_french}.mp3
 }
 generate_speeches
 
 
 upload_files() {
-    aws s3 cp "./s3"/index.csv s3://everyday-story/index.csv
+    aws s3 cp "${local_prefix}"/index.csv s3://everyday-story/index.csv
 
     declare -a book_languages=("chinese" "english" "french")
     declare -a story_types=("metadata" "chinese_version" "english_version" "french_version")
 
     for lang in "${book_languages[@]}"; do
         echo "Now uploading $lang book"
-        aws s3 cp "./s3/books/${lang}_chengyu.txt" "s3://everyday-story/books/${lang}_chengyu.txt"
+        aws s3 cp "${local_prefix}/books/${lang}_chengyu.txt" "s3://everyday-story/books/${lang}_chengyu.txt"
     done
 
     for type in "${story_types[@]}"; do
         echo "Now uploading mp3 and ${type} json files"
-        aws s3 cp "./s3/story/${index_value}_${type}_${title_chinese}.json" "s3://everyday-story/story/${index_value}_${type}_${title_chinese}.json"
-        aws s3 cp "./s3/story/${index_value}_${type}_${title_chinese}.mp3" "s3://everyday-story/story/${index_value}_${type}_${title_chinese}.mp3"
+        aws s3 cp "${local_prefix}/story/${index_value}_${type}_${title_chinese}.json" "s3://everyday-story/story/${index_value}_${type}_${title_chinese}.json"
+        aws s3 cp "${local_prefix}/story/${index_value}_${type}_${title_chinese}.mp3" "s3://everyday-story/story/${index_value}_${type}_${title_chinese}.mp3"
 
         echo "Now updating s3 objects tags"
         aws s3api put-object-tagging --bucket $bucket_name --key "story/${index_value}_${type}_${title_chinese}.json" --tagging 'TagSet=[{Key=language,Value=chinese}, {Key=scope,Value=成语}, {Key=metadata,Value=yes}]'
