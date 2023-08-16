854cab7400e61ef394333a9d00cd42379f6653da
anne <honghonghunter@hotmail.com>
2023-08-17 05:40:33 +1200

    古蜀国密码test2

--- chinese_script/generate_upload.sh
+++ chinese_script/generate_upload.sh
@@ -107,14 +107,14 @@ upload_files() {
     echo "Now uploading mp3 files."
     for type in "${story_types[@]}"; do
         echo "Now uploading ${type} mp3 files."
-        aws s3 cp "${local_prefix}/${s3_folder}/${index_value}_${type}_${title_chinese}.mp3" "s3://everyday-story/${s3_folder}/${index_value}_${type}_${title_chinese}.mp3"
+        aws s3 cp "${local_prefix}/story/${index_value}_${type}_${title_chinese}.mp3" "s3://everyday-story/${s3_folder}/${index_value}_${type}_${title_chinese}.mp3"
     done
 
     echo "Now updating file tags."
     for type in "${story_types[@]}"; do
         echo "Now updating s3 objects tags"
-        aws s3api put-object-tagging --bucket $bucket_name --key "${s3_folder}/${index_value}_metadata_${title_chinese}.json" --tagging 'TagSet=[{Key=language,Value=chinese}, {Key=scope,Value=成语}, {Key=metadata,Value=yes}]'
-        aws s3api put-object-tagging --bucket $bucket_name --key "${s3_folder}/${index_value}_${type}_${title_chinese}.mp3" --tagging 'TagSet=[{Key=language,Value=chinese}, {Key=scope,Value=成语}]'
+        aws s3api put-object-tagging --bucket $bucket_name --key "story/${index_value}_metadata_${title_chinese}.json" --tagging 'TagSet=[{Key=language,Value=chinese}, {Key=scope,Value=成语}, {Key=metadata,Value=yes}]'
+        aws s3api put-object-tagging --bucket $bucket_name --key "story/${index_value}_${type}_${title_chinese}.mp3" --tagging 'TagSet=[{Key=language,Value=chinese}, {Key=scope,Value=成语}]'
     done
 }
 upload_files "${local_prefix}"
