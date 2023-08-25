d9791bd57987185832551462cee419b7cbac6b79
anne <honghonghunter@hotmail.com>
2023-08-26 06:26:00 +1200

    chapter 38

--- chinese_script/generate_upload.sh
+++ chinese_script/generate_upload.sh
@@ -16,7 +16,7 @@ category="古蜀国密码"
 year=$(date +'%Y')
 day_of_year=$(date +'%j')
 task_time=$(date +'%H:%M:%S')
-i=381
+i=388
 
 # ====================Need to update when adding another language==================
 # Generate index value
