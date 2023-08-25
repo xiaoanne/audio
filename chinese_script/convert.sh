45b59c5986c082152685367dde95ebdf3152f164
anne <honghonghunter@hotmail.com>
2023-08-26 08:29:08 +1200

    chapter 42-copy the latest mp3 file

--- chinese_script/convert.sh
+++ chinese_script/convert.sh
@@ -30,7 +30,7 @@ latest_object=$(aws s3api list-objects-v2 \
 echo "The latest object in the $s3_folder folder is: $latest_object"
 
 # Copy the object with the new name
-aws s3 cp "s3://${s3_bucket}/${latest_object}" "s3://${s3_bucket}/${s3_folder}/古蜀国密码-${chapter}.mp3"
+aws s3 cp "s3://${s3_bucket}/${latest_object}" "s3://${s3_bucket}/${s3_folder}/gushuguomima-${chapter}.mp3"
 
 # Delete the original object
 aws s3 rm "s3://${s3_bucket}/${latest_object}"
