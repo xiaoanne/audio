14860b003461c930bbdb5bb92ba252724bcbf838
anne <honghonghunter@hotmail.com>
2023-08-29 02:48:10 +1200

    chapter 42-copy the latest mp3 file, test 1

--- chinese_script/generate_upload.sh
+++ chinese_script/generate_upload.sh
@@ -18,7 +18,7 @@ year=$(date +'%Y')
 day_of_year=$(date +'%j')
 task_time=$(date +'%H:%M:%S')
 #i=539
-chapter=$("古蜀国密码_chapter"42)
+chapter=$("古蜀国密码_chapter"+42)
 
 # ====================Need to update when adding another language==================
 # Generate index value
