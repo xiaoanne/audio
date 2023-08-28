b79aecd5ffb916d56e703fa142f4f1018f2537d5
anne <honghonghunter@hotmail.com>
2023-08-29 03:06:23 +1200

    chapter 42-remove latest s3 object for unrenamed object, test 1

--- chinese_script/convert.sh
+++ chinese_script/convert.sh
@@ -1,38 +1,58 @@
-#!/bin/bash
 
 # Common variable declarations
 file_path="./chinese_script/story_original.txt"
+local_prefix="./downloads"
 s3_bucket="everyday-story"
 s3_folder="gushuguomima"
 sample_rate=24000
 title_chinese=$(head -n 1 "$file_path")
-# ====================Need to update when adding another language==================
 story_chinese=$(sed -n '2,$p' "$file_path" | tr -d '[:space:]' | tr -d '\n')
-chapter=42
+category="古蜀国密码"
+year=$(date +'%Y')
+day_of_year=$(date +'%j')
+task_time=$(date +'%H:%M:%S')
+chapter_prefix="gushuguomima_chapter"
+chapter_number=42
+chapter="${chapter_prefix}${chapter_number}"
 
-# Start the speech synthesis task
-task_id=$(aws polly start-speech-synthesis-task \
+
+# Generate index value
+index_value="${category}_${year}_${day_of_year}_${task_time}"
+echo "Index value: ${index_value}"
+echo "Title value: ${title_chinese}"
+echo "Chinese story: ${story_chinese}"
+
+
+
+generate_speeches() {
+    echo "Generating story speeches."
+    aws polly synthesize-speech --text "$story_chinese" --output-format mp3 --voice-id Zhiyu --sample-rate $sample_rate "${local_prefix}/${s3_folder}/${index_value}_古蜀国密码_${title_chinese}.mp3"
+}
+
+
+aws polly start-speech-synthesis-task \
   --region ap-southeast-2 \
   --endpoint-url "https://polly.ap-southeast-2.amazonaws.com/" \
   --output-format mp3 \
-  --output-s3-bucket-name "${s3_bucket}" \
-  --output-s3-key-prefix "${s3_folder}/${chapter}" \
+  --output-s3-bucket-name everyday-story \
+  --output-s3-key-prefix ${s3_folder}/${chapter} \
   --voice-id Zhiyu \
-  --text "$story_chinese" \
-  --query 'SynthesisTask.TaskId' --output text)
+  --text "$story_chinese"
 
-echo "Speech synthesis task started with ID: $task_id"
+sleep 120
 
-# Wait for the task to complete
-aws polly wait speech-synthesis-task-completed --task-id "$task_id"
+# Get the list of objects in the specified folder, sorted by last modified time
+latest_object=$(aws s3api list-objects-v2 \
+  --bucket "$s3_bucket" \
+  --prefix "$s3_folder" \
+  --query "sort_by(Contents, &LastModified) | [-1].Key" \
+  --output text
+)
 
-# Retrieve the latest MP3 file after task completion
-latest_mp3=$(aws s3api list-objects --bucket "$s3_bucket" --prefix "${s3_folder}/${chapter}" --query 'Contents | sort_by(@, &LastModified) | [-1].Key' --output text)
-echo "The latest MP3 file after task completion is: $latest_mp3"
+echo "The latest object in the $s3_folder folder is: $latest_object"
 
 # Copy the object with the new name
-echo "Now copying to chapter mp3 file"
-aws s3 cp "s3://${s3_bucket}/${s3_folder}/.mp3" "s3://${s3_bucket}/${s3_folder}/${chapter}.mp3"
+aws s3 cp "s3://${s3_bucket}/${latest_object}" "s3://${s3_bucket}/${s3_folder}/${chapter}.mp3"
 
 # Delete the original object
-#aws s3 rm "s3://${s3_bucket}/${s3_folder}/.mp3"
+aws s3 rm "s3://${s3_bucket}/${latest_object}"
