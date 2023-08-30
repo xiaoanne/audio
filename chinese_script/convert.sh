ba6deb170983e7485d7b833436b835df8aa6b7af
honghong <honghonghunter@hotmail.com>
2023-08-31 10:02:15 +1200

    chapter-49-8

--- chinese_script/convert.sh
+++ chinese_script/convert.sh
@@ -2,8 +2,8 @@
 
 
 # Common variable declarations
-file_path="./chinese_script/story_original_49-7.txt"
-chapter_number="49-7"
+file_path="./chinese_script/story_original_49-8.txt"
+chapter_number="49-8"
 local_prefix="./downloads"
 s3_bucket="everyday-story"
 s3_folder="gushuguomima"
