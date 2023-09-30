cc4fb66d0c70aa1eb30dd8827bf61e314bf0fecc
Anne Wang <huiyang.wang621@gmail.com>
2023-09-30 09:33:41 +0000

    c

--- script/cy.sh
+++ script/cy.sh
@@ -9,11 +9,11 @@ sample_rate=24000
 title_chinese=$(head -n 1 "$file_path")
 title_english=$(sed -n '2p' "$file_path")
 story_chinese=$(sed -n '3,$p' "$file_path" | tr -d '[:space:]' | tr -d '\n')
-echo "${title_chinese}:" "${story_chinese}" > "${local_prefix}/books/story/chinese_${title_chinese}.txt"
-echo "${title_chinese}" >> "${local_prefix}/books/book_chinese.txt"
-echo "${story_chinese}" >> "${local_prefix}/books/book_chinese.txt"
-echo "${break_line}" >> "${local_prefix}/books/book_chinese.txt"
-aws polly synthesize-speech --text "投桃报李的故事，${story_chinese}" --output-format mp3 --voice-id Zhiyu --sample-rate "$sample_rate" "${local_prefix}/books/audio/chinese_${title_chinese}.mp3"
+echo "${title_chinese}:" "${story_chinese}" > "${local_prefix}/books/story/Chinese_${title_chinese}.txt"
+echo "${title_chinese}" >> "${local_prefix}/books/book_Chinese.txt"
+echo "${story_chinese}" >> "${local_prefix}/books/book_Chinese.txt"
+echo "${break_line}" >> "${local_prefix}/books/book_Chinese.txt"
+aws polly synthesize-speech --text "投桃报李的故事，${story_chinese}" --output-format mp3 --voice-id Zhiyu --sample-rate "$sample_rate" "${local_prefix}/books/audio/Chinese_${title_chinese}.mp3"
 
 # declare -a story_types=("chinese_version" "english_version" "french_version")
 declare -a book_languages=("english" "french")
@@ -50,11 +50,21 @@ generate_books() {
 
 generate_audio() {
     local audio_path="${local_prefix}/books/audio/"
-    aws polly synthesize-speech --text "The story of ${title_english}, ${English}" --output-format mp3 --voice-id Matthew --sample-rate $sample_rate "${audio_path}/English_${title_chinese}.mp3"
-    aws polly synthesize-speech --text "The story of ${title_english}, ${French}" --output-format mp3 --voice-id Celine --sample-rate $sample_rate "${audio_path}/French_${title_chinese}.mp3"
+    aws polly synthesize-speech --text "The story of ${title_english}, ${English}" --engine neural --output-format mp3 --voice-id Matthew --sample-rate $sample_rate "${audio_path}/English_${title_chinese}.mp3"
+    aws polly synthesize-speech --text "The story of ${title_english}, ${French}" --engine neural --output-format mp3 --voice-id Lea --sample-rate $sample_rate "${audio_path}/French_${title_chinese}.mp3"
+    aws polly synthesize-speech --text "The story of ${title_english}, ${Spanish}" --engine neural --output-format mp3 --voice-id Lucia --sample-rate $sample_rate "${audio_path}/Spanish_${title_chinese}.mp3"
+    aws polly synthesize-speech --text "The story of ${title_english}, ${Arabic}" --engine neural --output-format mp3 --voice-id Hala --sample-rate $sample_rate "${audio_path}/Arabic_${title_chinese}.mp3"
+
+    # aws polly synthesize-speech --text "The story of ${title_english}, ${French}" --output-format mp3 --voice-id Celine --sample-rate $sample_rate "${audio_path}/French_${title_chinese}.mp3"
+
+}
+
+upload_files() {
+    aws s3 cp "${local_prefix}"/books s3://everyday-story/chengyu --recursive
 }
 
 # Call the function
 generate_books
 generate_audio
+upload_files
 
